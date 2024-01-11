bdbb3242980e3c29eb0ece60a0f06223342d543a
Admin <Admin@DESKTOP-OT2CRA5>
2024-01-11 16:02:48 +0100

    Commit 2

--- src/CreandoRepositorio.java
+++ src/CreandoRepositorio.java
@@ -9,6 +9,9 @@ public class CreandoRepositorio {
     
     
         public static void main(String[] args) {
+  System.out.println("linea de codigo local");
+  // comentario del commit
+  
   
     }
     
